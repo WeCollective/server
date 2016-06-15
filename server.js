17694030db5cec2051b0f2691247bfa2764b20a0
MikeC1995 <mike_christensen@btinternet.com>
2016-06-15 13:52:48 +0100

    dummy put to the DynamoDB Users table

--- server.js
+++ server.js
@@ -4,6 +4,39 @@
 var express    = require('express');          // call express
 var app        = express();                   // define our app using express
 var bodyParser = require('body-parser');      // reading request bodies
+var AWS = require("aws-sdk");                 // interfacing with AWS
+console.log("Keys:");
+console.log(process.env.AWS_ACCESS_KEY_ID_WECO_API);
+console.log(process.env.AWS_SECRET_ACCESS_KEY_WECO_API);
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_WECO_API,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_WECO_API,
+  region: "eu-west-1",
+  sslEnabled: true,
+  logger: process.stdout
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+var table = "Users";
+var year = 2015;
+var title = "The Big New Movie";
+
+var params = {
+  TableName: "Users",
+  Item: {
+    "id": 1,
+    "username": "TestUser1"
+  }
+};
+
+console.log("Adding a new item...");
+docClient.put(params, function(err, data) {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err));
+    } else {
+      console.log("Success!");
+    }
+});
 
 // SET ENVIRONMENT AND PORT
 var env = (process.env.NODE_ENV || "development");

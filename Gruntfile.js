c8c446110969fe6bd4d17bfaeaf3fb66972cc5b6
MikeC1995 <mike_christensen@btinternet.com>
2016-06-16 15:31:29 +0100

    added grunt serve (local), linting, publish and deploy to environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,76 @@
 module.exports = function(grunt) {
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-exec');
+
+  // Configure tasks
+  grunt.initConfig({
+    // javascript linting
+    jshint: {
+      files: ['Gruntfile.js', 'server.js', 'public/**/*.js'],
+      options: {
+        node: true, // tell jshint we are using nodejs to avoid incorrect errors
+        globals: {  // list of global variables and whether they are assignable
+          "angular": false
+        }
+      }
+    },
+    // File watching
+    watch: {
+      js: {
+        files: ['**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    // Live reload server (for local development)
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+    // concurrently run the server and watch for file changes
+    concurrent: {
+      serve: {
+        options: {
+          logConcurrentOutput: true
+        },
+        tasks: ['nodemon', 'watch']
+      }
+    },
+    // execute shell commands
+    exec: {
+      publish: 'git checkout production && git merge master && git checkout master',
+      deploy: {
+        cmd: function(environment) {
+          var checkout;
+          if(environment == 'development') {
+            checkout = 'master';
+          } else if(environment == 'production') {
+            checkout = 'production';
+          } else {
+            return '';
+          }
+          return 'echo Checking out ' + checkout + ' && git checkout ' + checkout + ' && echo Deploying... && eb deploy && git checkout master';
+        }
+      }
+    }
+  });
+
+  /* Register main tasks.
+  **    grunt serve           locally serve the web app and simultaneously watch for file changes for linting
+  **    grunt jshint          lint all js files
+  **    grunt publish         merges development branch into production
+  **    grunt deploy:env      builds the current branch, and deploys to the specified environment
+  **                          (either "development" or "production"), merging into production if needed.
+  */
+  grunt.registerTask('serve', 'concurrent:serve');
+  grunt.registerTask('publish', 'exec:publish');
+  grunt.registerTask('deploy:development', ['jshint', 'exec:deploy:development']);
+  grunt.registerTask('deploy:production', ['jshint', 'publish', 'exec:deploy:production']);
 };

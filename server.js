2ec607526a2f77ee4d85b12eafd11413294e14cb
MikeC1995 <mike_christensen@btinternet.com>
2016-06-22 15:09:43 +0100

    CORS

--- server.js
+++ server.js
@@ -33,6 +33,13 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// CROSS ORIGIN RESOURCE SHARING
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // AUTHENTICATION AND SESSION MANAGEMENT
 var options = {
     table: db.Table.Sessions,

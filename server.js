3160328287172638ebeb24db0f1a66590d8f011f
MikeC1995 <mike_christensen@btinternet.com>
2016-06-22 16:56:07 +0100

    CORS credentials

--- server.js
+++ server.js
@@ -37,7 +37,9 @@ app.use(bodyParser.json());
 // CROSS ORIGIN RESOURCE SHARING
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
   next();
 });
 

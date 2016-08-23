c7a8ccd8717f29a9820abfc2725036bd66d600e9
Michael Christensen <Michael Christensen>
2016-08-23 15:11:25 +0100

    added child branch rquest and posts docs

--- routers/branch-posts/branch-posts.router.js
+++ routers/branch-posts/branch-posts.router.js
@@ -8,13 +8,103 @@ module.exports = function(app, passport) {
   var controller = require('./branch-posts.controller.js');
 
   router.route('/')
-    // get all posts on the branch
+    /**
+     * @api {get} /branch/:branchid/posts>timeafter=<timeafter>&stat=<stat> Get Branch Posts
+     * @apiName Get Branch Posts
+     * @apiGroup Posts
+     * @apiPermission guest
+     *
+     * @apiParam (URL Parameters) {String} branchid Branch unique id.
+     * @apiParam (Query Parameters) {Number} timeafter Only fetch posts created after this time (UNIX timestamp). [optional; default 0]
+     * @apiParam (Query Parameters) {String} stat The stat type to sort the posts by ['global', 'local', 'individual'] [optional; default 'individual']
+     *
+     * @apiSuccess (Successes) {String} data An array of post objects.
+     * @apiSuccessExample {json} SuccessResponse:
+     *  HTTP/1.1 200
+     *     {
+     *       "message": "Success",
+     *       "data": [
+     *         {
+     *           "id": "johndoe-1471868592207",
+     *           "date": 1471868592207,
+     *           "individual": 0,
+     *           "branchid": "science",
+     *           "type": "text"
+     *         },
+     *         {
+     *          "id": "johndoe-1471630511498",
+     *          "date": 1471630511498,
+     *          "individual": 0,
+     *          "branchid": "science",
+     *          "type": "video"
+     *        },
+     *        {
+     *          "id": "johndoe-1471889497552",
+     *          "date": 1471889497552,
+     *          "individual": 0,
+     *          "branchid": "science",
+     *          "type": "image"
+     *        }
+     *      ]
+     *    }
+     *
+     * @apiUse BadRequest
+     * @apiUse InternalServerError
+     */
     .get(controller.get);
 
   router.route('/:postid')
-    // get a specific post on the branch
+    /**
+     * @api {get} /branch/:branchid/posts/:postid Get Branch Post
+     * @apiName Get Branch Post
+     * @apiGroup Posts
+     * @apiPermission guest
+     *
+     * @apiParam (URL Parameters) {String} branchid Branch unique id.
+     * @apiParam (URL Parameters) {String} postid Post unique id.
+     *
+     * @apiSuccess (Successes) {String} data An array containing a single post object
+     * @apiSuccessExample {json} SuccessResponse:
+     *  HTTP/1.1 200
+     *     {
+     *       "message": "Success",
+     *       "data": [
+     *          {
+     *            "id": "johndoe-1471630511498",
+     *            "down": 0,
+     *            "individual": 0,
+     *            "branchid": "science",
+     *            "rank": 0,
+     *            "local": 0,
+     *            "date": 1471630511498,
+     *            "up": 0,
+     *            "type": "video"
+     *          }
+     *       ]
+     *    }
+     *
+     *
+     * @apiUse NotFound
+     * @apiUse BadRequest
+     * @apiUse InternalServerError
+     */
     .get(controller.getPost)
-    // vote on a post
+
+    /**
+     * @api {put} /branch/:branchid/posts/:postid Vote on Post
+     * @apiName Vote on Post
+     * @apiGroup Posts
+     * @apiPermission auth
+     *
+     * @apiParam (URL Parameters) {String} branchid Branch unique id.
+     * @apiParam (URL Parameters) {String} postid Post unique id.
+     * @apiParam (Body Parameters) {String} vote Vote direction ['up', 'down']
+     *
+     * @apiUse OK
+     * @apiUse NotFound
+     * @apiUse BadRequest
+     * @apiUse InternalServerError
+     */
     .put(ACL.validateRole(ACL.Roles.AuthenticatedUser), controller.put);
 
   return router;
--- routers/requests/subbranches.router.js
+++ routers/requests/subbranches.router.js
@@ -9,15 +9,69 @@ module.exports = function(app, passport) {
 
   // get subbranch requests
   router.route('/')
+    /**
+     * @api {get} /branch/:branchid/requests/subbranches Get Child Branch Requests
+     * @apiName Get Child Branch Requests
+     * @apiGroup Requests
+     * @apiPermission mod
+     *
+     * @apiParam (URL Parameters) {String} branchid Branch unique id.
+     *
+     * @apiSuccess (Successes) {String} data An array of child branch request objects.
+     * @apiSuccessExample {json} SuccessResponse:
+     *  HTTP/1.1 200
+     *     {
+     *       "message": "Success",
+     *       "data": [
+     *         {
+     *           "parentid": "science",
+     *           "date": 1471961075596,
+     *           "creator": "johndoe",
+     *           "childid": "physics"
+     *         }
+     *      ]
+     *    }
+     *
+     * @apiUse NotFound
+     * @apiUse InternalServerError
+     */
     .get(function(req, res, next) {
       ACL.validateRole(ACL.Roles.Moderator, req.params.branchid)(req, res, next);
     }, controller.get);
 
-  // create and update a subbranch request
   router.route('/:childid')
+    /**
+     * @api {post} /branch/:branchid/requests/subbranches/:childid Create Child Branch Request
+     * @apiName Create Child Branch Request
+     * @apiGroup Requests
+     * @apiPermission mod
+     *
+     * @apiParam (URL Parameters) {String} branchid Branch unique id.
+     * @apiParam (URL Parameters) {String} childid Child Branch unique id.
+     *
+     * @apiUse OK
+     * @apiUse BadRequest
+     * @apiUse NotFound
+     * @apiUse InternalServerError
+     */
     .post(function(req, res, next) {
       ACL.validateRole(ACL.Roles.Moderator, req.params.childid)(req, res, next);
     }, controller.post)
+    /**
+     * @api {put} /branch/:branchid/requests/subbranches/:childid Answer Child Branch Request
+     * @apiName Answer Child Branch Request
+     * @apiGroup Requests
+     * @apiPermission mod
+     *
+     * @apiParam (URL Parameters) {String} branchid Branch unique id.
+     * @apiParam (URL Parameters) {String} childid Child Branch unique id.
+     * @apiParam (Body Parameters) {String} action The action to take ['accept', 'reject']
+     *
+     * @apiUse OK
+     * @apiUse BadRequest
+     * @apiUse NotFound
+     * @apiUse InternalServerError
+     */
     .put(function(req, res, next) {
       ACL.validateRole(ACL.Roles.Moderator, req.params.branchid)(req, res, next);
     }, controller.put);

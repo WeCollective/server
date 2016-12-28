ef340d7f278d769de6bb766d74d70836791c27c0
Michael Christensen <Michael Christensen>
2016-12-28 12:16:35 +0000

    debug prints

--- config/mailer.js
+++ config/mailer.js
@@ -76,13 +76,15 @@ module.exports = {
       if(user.dob) request.body[0]["dob"] = mmddyyyy(new Date(user.dob));
 
       console.log("USER ::: \n%j", user);
-      console.log("REQ  ::: \n%j", request.body[0]);
 
       request.method = update ? 'PATCH' : 'POST';
       request.path = '/v3/contactdb/recipients';
 
+      console.log("REQ  ::: \n%j", request);
       sendgrid.API(request, function (error, response) {
+        console.log("ERR  ::: \n%j", error);
         if(error) reject();
+        console.log("RES  ::: \n%j", response);
         if(JSON.parse(response.body).error_count > 0) reject();
         resolve();
       });

7414c1a6f47813a14f9ab394406194d33850f702
Lubos <lmenus@lmen.us>
2017-07-16 20:35:01 +0100

    Fix not being able to vote on polls

--- models/poll-answer.model.js
+++ models/poll-answer.model.js
@@ -18,61 +18,12 @@ var PollAnswer = function(data) {
 PollAnswer.prototype = Object.create(Model.prototype);
 PollAnswer.prototype.constructor = PollAnswer;
 
-// Validate the properties specified in 'properties' on the PollAnswer object,
-// returning an array of any invalid ones
-PollAnswer.prototype.validate = function (properties) {
-  let invalids = [];
-
-  // ensure id exists and is of correct length
-  if (properties.includes('id')) {
-    if (!validate.pollanswerid(this.data.id)) {
-      invalids.push('id');
-    }
-  }
-
-  // ensure postid exists and is of correct length
-  if (properties.includes('postid')) {
-    if (!validate.postid(this.data.postid)) {
-      invalids.push('postid');
-    }
-  }
-
-  // ensure votes exists and is a valid number
-  if (properties.includes('votes')) {
-    if (isNaN(this.data.votes)) {
-      invalids.push('votes');
-    }
-  }
-
-  // ensure title is valid
-  if (properties.includes('text')) {
-    if (!this.data.text || this.data.text.length < 1 || this.data.text.length > 300) {
-      invalids.push('text');
-    }
-  }
-
-  // ensure creator is valid username
-  if (properties.includes('creator')) {
-    if (!validate.username(this.data.creator)) {
-      invalids.push('creator');
-    }
-  }
-
-  // ensure creation date is valid
-  if (properties.includes('date')) {
-    if (!validate.date(this.data.date)) {
-      invalids.push('date');
-    }
-  }
-
-  return invalids;
-};
-
 // Get a PollAnswer by its id from the db, and
 // instantiate the object with this data.
 // Rejects promise with true if database error, with false if no post found.
 PollAnswer.prototype.findById = function (id) {
   const self = this;
+
   return new Promise((resolve, reject) => {
     aws.dbClient.get({
       Key: { id },
@@ -87,7 +38,7 @@ PollAnswer.prototype.findById = function (id) {
       }
 
       self.data = data.Item;
-      return resolve();
+      return resolve(self.data);
     });
   });
 };
@@ -135,6 +86,56 @@ PollAnswer.prototype.findByPost = function (postid, sortBy, last) {
       return resolve(data.Items.slice(0, limit));
     });
   });
+};
+
+// Validate the properties specified in 'properties' on the PollAnswer object,
+// returning an array of any invalid ones
+PollAnswer.prototype.validate = function (props) {
+  const invalids = [];
+
+  // ensure id exists and is of correct length
+  if (props.includes('id')) {
+    if (!validate.pollanswerid(this.data.id)) {
+      invalids.push('id');
+    }
+  }
+
+  // ensure postid exists and is of correct length
+  if (props.includes('postid')) {
+    if (!validate.postid(this.data.postid)) {
+      invalids.push('postid');
+    }
+  }
+
+  // ensure votes exists and is a valid number
+  if (props.includes('votes')) {
+    if (isNaN(this.data.votes)) {
+      invalids.push('votes');
+    }
+  }
+
+  // ensure title is valid
+  if (props.includes('text')) {
+    if (!this.data.text || this.data.text.length < 1 || this.data.text.length > 300) {
+      invalids.push('text');
+    }
+  }
+
+  // ensure creator is valid username
+  if (props.includes('creator')) {
+    if (!validate.username(this.data.creator)) {
+      invalids.push('creator');
+    }
+  }
+
+  // ensure creation date is valid
+  if (props.includes('date')) {
+    if (!validate.date(this.data.date)) {
+      invalids.push('date');
+    }
+  }
+
+  return invalids;
 };
 
 module.exports = PollAnswer;
--- routers/poll/controller.js
+++ routers/poll/controller.js
@@ -5,7 +5,7 @@ const PollAnswer = require('../../models/poll-answer.model');
 const Post = require('../../models/post.model');
 const PostData = require('../../models/post-data.model');
 const success = require('../../responses/successes');
-const UserVote = require('../../models/user-vote.model');
+const Vote = require('../../models/user-vote.model');
 
 module.exports = {
   get(req, res) {
@@ -121,52 +121,66 @@ module.exports = {
   },
 
   votePoll(req, res) {
-    if (!req.params.postid) {
+    const answer = new PollAnswer();
+    const answerId = req.params.answerid;
+    const newVoteDirection = req.body.vote;
+    const postId = req.params.postid;
+    const username = req.user.username;
+
+    const itemId = `poll-${postId}`;
+
+    if (!postId) {
       return error.BadRequest(res, 'Missing postid');
     }
 
-    if (!req.params.answerid) {
+    if (!answerId) {
       return error.BadRequest(res, 'Missing answerid');
     }
 
-    if (req.body.vote !== 'up') {
+    // Poll votes are "up" by default. This has no effect at the moment, just
+    // to fit the item into the database structure.
+    if (newVoteDirection !== 'up') {
       return error.BadRequest(res, 'Missing or malformed vote parameter');
     }
-
-    const updatedAnswer = new PollAnswer();
-    updatedAnswer
-      .findById(req.params.answerid)
+    
+    answer
+      .findById(answerId)
       .then(() => {
-        if (updatedAnswer.data.postid !== req.params.postid) {
-          return error.NotFound(res);
+        if (answer.data.postid !== postId) {
+          return Promise.reject({ code: 404 });
         }
 
-        const uservote = new UserVote();
-        return uservote.findByUsernameAndItemId(req.user.username, `poll-${req.params.postid}`);
-      }, () => {
-        return error.NotFound(res);
+        return new Vote().findByUsernameAndItemId(username, itemId);
       })
-      .then(() => {
-        // user has voted on this poll before
-        return error.BadRequest(res, 'User has already voted on this poll');
-      }, () => {
-        // user has not voted on this poll before
-        updatedAnswer.set('votes', updatedAnswer.data.votes + 1);
-        return updatedAnswer.update();
-      })
-      .then(response => {
-        const vote = new UserVote({
-          direction: req.body.vote,
-          itemid: `poll-${req.params.postid}`,
-          username: req.user.username,
+      .then(existingVoteData => {
+        if (existingVoteData) {
+          return Promise.reject({
+            code: 400,
+            message: 'User has already voted on this poll',
+          });
+        }
+
+        const vote = new Vote({
+          direction: newVoteDirection,
+          itemid: itemId,
+          username,
         });
 
         return vote.save();
+      })
+      .then(() => {
+        answer.set('votes', answer.data.votes + 1);
+        return answer.update();
       })
       .then(() => success.OK(res))
       .catch(err => {
         if (err) {
           console.error('Error voting on poll answer:', err);
+
+          if (typeof err === 'object' && err.code) {
+            return error.code(res, err.code, err.message);
+          }
+
           return error.InternalServerError(res);
         }
 

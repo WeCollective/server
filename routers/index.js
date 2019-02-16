28d7fed6825a1a19a6bb4a6e635d3310ef6846ef
Lubos <lmenus@lmen.us>
2019-02-16 18:15:08 +0000

    Remove search API references

--- routers/index.js
+++ routers/index.js
@@ -77,7 +77,6 @@ app.use(`${version}/constant`, getRouter('constant'));
 app.use(`${version}/poll`, getRouter('poll'));
 app.use(`${version}/post`, getRouter('post'));
 app.use(`${version}/scraper`, getRouter('scraper'));
-app.use(`${version}/search`, getRouter('search'));
 app.use(`${version}/slack`, getRouter('slack'));
 app.use(`${version}/user`, getRouter('user'));
 

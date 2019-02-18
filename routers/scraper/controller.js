f4e265ca9be6f3187bc2afae15f4716e13862950
Lubos <lmenus@lmen.us>
2019-02-18 21:34:58 +0000

    Attempt to catch bad urls in scraper

--- routers/scraper/controller.js
+++ routers/scraper/controller.js
@@ -1,62 +1,73 @@
-const reqlib = require('app-root-path').require;
-const scrape = require('html-metadata');
+const reqlib = require('app-root-path').require
+const scrape = require('html-metadata')
 
-const Constants = reqlib('config/constants');
+const Constants = reqlib('config/constants')
 
-const { PostTypePage } = Constants;
-const { url: UrlPolicy } = Constants.Policy;
+const { PostTypePage } = Constants
+const { url: UrlPolicy } = Constants.Policy
 
-module.exports.scrap = (req, res, next) => {
-  const { url } = req.query;
+/**
+ * @param {string} [type]
+ *
+ * @returns {string}
+ */
+const normalizePostType = type => {
+  switch (type) {
+    case 'article':
+      return PostTypePage
+    default:
+      return ''
+  }
+}
 
-  if (!url) {
-    req.error = {
+/**
+ * Attempts to return the original url if our scraper failed us and returned
+ * bad url. We define bad urls as urls which are subclasses of the original
+ * value, e.g. root domains.
+ *
+ * @param {string} original
+ * @param {string} [scraped='']
+ *
+ * @returns {string} One of the passed urls.
+ */
+const normalizeUrl = (original, scraped = '') => {
+  if (scraped && original.includes(scraped)) return original
+  return scraped
+}
+
+module.exports.scrap = async (req, res, next) => {
+  try {
+    const { url } = req.query
+
+    if (!url) throw {
       message: 'Missing url.',
       status: 400,
-    };
-    return next(JSON.stringify(req.error));
-  }
+    }
 
-  const path = url.includes('http') ? url : `https://${url}`;
-
-  if (!UrlPolicy.test(url)) {
-    req.error = {
+    if (!UrlPolicy.test(url)) throw {
       message: 'Incorrect url.',
       status: 400,
-    };
-    return next(JSON.stringify(req.error));
+    }
+
+    const path = url.includes('http') ? url : `https://${url}`
+    const data = await scrape(path)
+
+    const general = data.general || {}
+    const openGraph = data.openGraph || {}
+    const type = normalizePostType(openGraph.type || general.type)
+
+    res.locals.data = {
+      text: openGraph.description || general.description || '',
+      title: openGraph.title || general.title || '',
+      type,
+      url: normalizeUrl(url, openGraph.url || general.url || general.canonical),
+    }
+    next()
+  }
+  catch (e) {
+    const error = typeof e === 'object' && e.status ? e : { message: e }
+    req.error = error
+    next(JSON.stringify(req.error))
   }
 
-  return scrape(path)
-    .then(data => {
-      const general = data.general || {};
-      const openGraph = data.openGraph || {};
-      let type = openGraph.type || general.type || '';
-
-      switch (type) {
-        case 'article':
-          type = PostTypePage;
-          break;
-
-        default:
-          type = '';
-          break;
-      }
-
-      const response = {
-        text: openGraph.description || general.description || '',
-        title: openGraph.title || general.title || '',
-        type,
-        url: openGraph.url || general.url || general.canonical || '',
-      };
-      return Promise.resolve(response);
-    })
-    .catch(err => {
-      console.log(err);
-      return Promise.resolve();
-    })
-    .then(data => {
-      res.locals.data = data;
-      return next();
-    });
-};
+}
